f3f4cae6786f109dbc6b5ed0ec5d2f007555e4c8
Torubiri Walter <toruwalt1997@gmail.com>
2024-01-18 08:43:15 +0100

    Sorting Task 1: PC to Mobile

---
+++ 0-bubble_sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * bubble_sort - A function that sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: A pointer to the array of numbers
+ * @size: The size of the array
+ * Return - 1 Always true
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+
+}
